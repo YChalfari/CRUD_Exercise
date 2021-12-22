657fb0c285c2a89b865c3ef6aeeb17ef0009ee63
Yitz <chalfari@gmail.com>
2021-12-22 17:18:40 +0200

    My very buggy crud api application

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 class Input extends Component {
+  state = { value: this.props.value };
+  handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.props.onChange(name, value);
+    this.setState({ value });
+  };
   render() {
     return (
       <input
         name={this.props.name}
         type="text"
         value={this.props.value}
-        onChange={this.props.onChange}
+        onChange={this.handleChange}
       />
     );
   }
